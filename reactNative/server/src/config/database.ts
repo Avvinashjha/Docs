46c057d99890bf6be1b8169ad5165b8f6767ed72
avinash.jh <avvinashjha@gmail.com>
2025-11-08 19:37:26 +0530

    sensitive content protected

--- reactNative/server/src/config/database.ts
+++ reactNative/server/src/config/database.ts
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 export const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'avinash.jh',
-  database: process.env.DB_NAME || 'express_app',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
